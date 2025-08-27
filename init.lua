f43ded824b8e677fa825340002e6c822d9fb0d78
Filip <fbudzynski56@gmail.com>
2025-08-27 23:08:47 +0200

    changes 27-08

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
 require("config.lazy")
-
--- vim.lsp.config("ruff", {
---   init_options = {
---     settings = {
---       -- Ruff language server settings go here
---     },
---   },
--- })
--- vim.lsp.enable("ruff")
